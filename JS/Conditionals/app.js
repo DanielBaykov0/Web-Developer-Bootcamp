eb290d8a5bf86df8b16d11526be737e3a0a933d0
Daniel Baykov <danibaikov@gmail.com>
2023-12-12 16:12:00 +0200

    adding conditionals

---
+++ JS/Conditionals/app.js
@@ -0,0 +1,17 @@
+// let random = Math.random();
+// if (random < 0.5) {
+//     console.log("YOUR NUMBER IS LESS THAN 0.5")
+//     console.log(random);
+// }
+
+// if (random >= 0.5) {
+//     console.log("YOUR NUMBER IS GREATER THAN 0.5")
+//     console.log(random);
+// }
+
+const dayOfWeek = "Monday";
+if (dayOfWeek === "Monday") {
+  console.log("I HAVE MONDAYS!");
+} else if (dayOfWeek === "Saturday") {
+  console.log("YAY I LOVE SATURDAYS");
+}
